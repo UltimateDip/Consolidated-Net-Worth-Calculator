2d5cae45659f78e42a700eb4e3abe869c9845c86
Dipankar <dipankardutta1234567@gmail.com>
2026-04-22 11:07:26 +0530

    feat: implement secure password change functionality with API and UI support

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -3,7 +3,10 @@ const authController = require('../controllers/auth.controller');
 
 const router = express.Router();
 
+const { authMiddleware } = require('../middlewares/auth.middleware');
+
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/change-password', authMiddleware, authController.changePassword);
 
 module.exports = router;
